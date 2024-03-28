725facd7c39b1f6c316685fa66422093ba150894
Patrick642 <109094173+Patrick642@users.noreply.github.com>
2024-03-28 19:54:40 +0100

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+// App paths
+define('ROOT_DIR', __DIR__ . '/../');
+define('BASE_URL', '/');
+
+// Includes
+define('PAGE_HEADER', ROOT_DIR . '/src/View/inc/header.phtml');
+define('PAGE_NAVBAR', ROOT_DIR . '/src/View/inc/navbar.phtml');
+define('PAGE_FOOTER', ROOT_DIR . '/src/View/inc/footer.phtml');
+
+// Database
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
